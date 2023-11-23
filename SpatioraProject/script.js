798ae1e4dd0fc50c95cae1fb5ff79175d3cdf102
nielsenlu2 <nielsen101004@gmail.com>
2023-11-22 23:23:33 -0700

    fixed spatioraproject

--- SpatioraProject/script.js
+++ SpatioraProject/script.js
@@ -1,5 +1,5 @@
 var projectRowFetched;
-fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vTsTHf1LQTApfz-DfLzXOxJGrMixgIVFiPuAhL_FQrGR1b38rUV4BtN1QnabNTSjVo0yCIve66zo2uP/pub?gid=471989265&single=true&output=tsv")
+fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vTsTHf1LQTApfz-DfLzXOxJGrMixgIVFiPuAhL_FQrGR1b38rUV4BtN1QnabNTSjVo0yCIve66zo2uP/pub?gid=2051244660&single=true&output=tsv")
 		.then((res) => res.text())
 		.then((text) => {
             projectRowFetched = text;
