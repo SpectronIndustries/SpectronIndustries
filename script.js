0474b364545ecdea1286812eee399536590db311
nielsenlu2 <nielsen101004@gmail.com>
2023-12-25 22:00:57 -0700

    Added Legacy Website to shutdown REPLIT

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var str;
 var contentStr;
 var orStr = str;
 var rowAmount;
-var pageAmount;
+var pageAmount = 4;
 var pageNumber = 1;
 var index = 0;
 var debugMode = true;
@@ -63,6 +63,8 @@ function startJS() {
 				 <a tabindex="8" class="menu-page" href="/ElectricLighters/">Elecric Lighters</a>
 				 <hr class="menu-page" width="100%">
 				 <a tabindex="8" class="menu-page" href="/MoreProjects/">More Projects</a>
+				 <!--<hr class="menu-page" width="100%">
+				 <a tabindex="8" class="menu-page" href="/Legacy/">Legacy</a>-->
 				 <hr width="100%">
 				 <a tabindex="9" href="/AboutUs/">ABOUT US</a>
 				 <hr width="100%">
@@ -84,6 +86,7 @@ function setPageText() {
 
 /* ===== Home Page ===== */
 function createContent() {
+	deleteContent();
 	/* ===== Set variables ===== */
 	// str = orStr;
 	// str = str.slice(str.indexOf("@Amount: "));
@@ -94,7 +97,13 @@ function createContent() {
 	// str = orStr;
 	// str = str.slice(str.indexOf("@PageAmount: "));
 	// pageAmount = parseInt(str.slice(str.indexOf("@PageAmount: ") + 13, str.indexOf("\n")));
-	pageAmount = 7;
+	pageAmount = pageAmount;
+
+	if(pageNumber == 1) {
+		index = amount;
+	} else {
+		index = (rowAmount - (pageAmount * (pageNumber - 1)));
+	}
 
 	// str = orStr;
 	// str = str.slice(str.indexOf("@NullBack: "));
@@ -105,9 +114,10 @@ function createContent() {
 	// str = str.slice(str.indexOf("@ImgGradient: "));
 	// var imgGradient = str.slice(str.indexOf("@ImgGradient: ") + 14, str.indexOf("\n"));
 	var imgGradient = true;
-
+	
 	/* ===== Log variables ===== */
 	if (debugMode == true) {
+		console.log("\n======================================== PAGE " + pageNumber + " ========================================\n\n");
 		console.log("Index: [" + index + "]");
 		console.log("Amount: [" + amount + "]");
 		console.log("PageAmount: [" + pageAmount + "]");
@@ -121,197 +131,204 @@ function createContent() {
 	if (!(index - ((pageAmount) * (pageNumber - 1)) < pageAmount)) {
 		console.log("========== ERROR ==========");
 	}
-
-	for(index = amount; (index - (pageAmount)) > 0;) {
-		for (index = amount - 1; (index - (pageAmount * pageNumber)) > (amount - (pageAmount * (pageNumber + 1))); index--) {
-			//index = forIndex;
-			/* ===== Set stri/=ng to original string ===== */
-			// str = orStr;
-
-			/* ===== Set string to current row ====== */
-			// str = str.slice(str.indexOf("#" + index));
-
-			/* ===== Set labels ===== */
-
-			timestamp = timestampArr = getArray("Timestamp", contentStr)[index].toLowerCase();
-			title = titleArr = getArray("Title", contentStr)[index];
-			desc = descArr = getArray("Desc", contentStr)[index];
-			date = dateArr = getArray("Date", contentStr)[index];
-			align = alignArr = getArray("Align", contentStr)[index];
-			imgSrc = imgSrcArr = getArray("ImgSrc", contentStr)[index];
-			btn = btnArr = getArray("Btn", contentStr)[index];
-			link = linkArr = getArray("Link", contentStr)[index];
-			flip = flipArr = getArray("Flip", contentStr)[index];
-
-			// var title = str.slice(str.indexOf("Title: ") + 7, str.indexOf("Desc: ") - 1);
-			// var desc = str.slice(str.indexOf("Desc: ") + 6, str.indexOf("Date: ") - 1);
-			// var date = str.slice(str.indexOf("Date: ") + 6, str.indexOf("Align: ") - 1);
-			// var align = str.slice(str.indexOf("Align: ") + 7, str.indexOf("ImgSrc: ") - 1);
-			// var imgSrc = str.slice(str.indexOf("ImgSrc: ") + 8, str.indexOf("Btn: ") - 1);
-			// var btn = str.slice(str.indexOf("Btn: ") + 5, str.indexOf("Link: ") - 1);
-			// var link = str.slice(str.indexOf("Link: ") + 6, str.indexOf("Flip: ") - 1);
-			// var flip = str.slice(str.indexOf("Flip: ") + 6, str.indexOf("#E") - 1);
-
-			if (timestamp != "@x") {
-				/* ===== Log labels ===== */
+	
+	for(index = index; (index - (pageAmount)) > (amount - (pageAmount * (pageNumber + 1)));) {
+		for (index = index; (index - (pageAmount)) > (amount - (pageAmount * (pageNumber + 1))); index--) {
+			// if(index >= 0) {
 				if (debugMode == true) {
-					console.log("========== Row " + index + " ==========");
-					console.log("Title: [" + title + "]");
-					console.log("Desc: [" + desc + "]");
-					console.log("Date: [" + date + "]");
-					console.log("Align: [" + align + "]");
-					console.log("ImgSrc: [" + imgSrc + "]");
-					console.log("Btn: [" + btn + "]");
-					console.log("Link: [" + link + "]");
-					console.log("Flip: [" + flip + "]");
+					console.log("---------- START ----------");
+					// console.log(getArray("Title", contentStr)[index]);
 				}
-
-
-				/* ===== Create row ===== */
-				var row = document.createElement('div');
-				row.classList.add('row');
-				row.id = 'row' + index;
-
-				/* ===== Create background image ===== */
-				var imgContainer = document.createElement('div');
-				imgContainer.classList.add('img-container');
-				var img = document.createElement('img');
-				/* ===== Set image to color or null image if no image avalible ===== */
-				if (imgSrc == "") {
-					if (nullBack == "Color") {
-						img.style.backgroundColor = "black";
-					} else if (nullBack == "Img") {
-						imgSrc = "../Assets/Img/Projects/nullImg.png";
+				//index = forIndex;
+				/* ===== Set stri/=ng to original string ===== */
+				// str = orStr;
+	
+				/* ===== Set string to current row ====== */
+				// str = str.slice(str.indexOf("#" + index));
+	
+				/* ===== Set labels ===== */
+	
+				timestamp = timestampArr = getArray("Timestamp", contentStr)[index - 1].toLowerCase();
+				title = titleArr = getArray("Title", contentStr)[index - 1];
+				desc = descArr = getArray("Desc", contentStr)[index - 1];
+				date = dateArr = getArray("Date", contentStr)[index - 1];
+				align = alignArr = getArray("Align", contentStr)[index - 1];
+				imgSrc = imgSrcArr = getArray("ImgSrc", contentStr)[index - 1];
+				btn = btnArr = getArray("Btn", contentStr)[index - 1];
+				link = linkArr = getArray("Link", contentStr)[index - 1];
+				flip = flipArr = getArray("Flip", contentStr)[index - 1];
+	
+				// var title = str.slice(str.indexOf("Title: ") + 7, str.indexOf("Desc: ") - 1);
+				// var desc = str.slice(str.indexOf("Desc: ") + 6, str.indexOf("Date: ") - 1);
+				// var date = str.slice(str.indexOf("Date: ") + 6, str.indexOf("Align: ") - 1);
+				// var align = str.slice(str.indexOf("Align: ") + 7, str.indexOf("ImgSrc: ") - 1);
+				// var imgSrc = str.slice(str.indexOf("ImgSrc: ") + 8, str.indexOf("Btn: ") - 1);
+				// var btn = str.slice(str.indexOf("Btn: ") + 5, str.indexOf("Link: ") - 1);
+				// var link = str.slice(str.indexOf("Link: ") + 6, str.indexOf("Flip: ") - 1);
+				// var flip = str.slice(str.indexOf("Flip: ") + 6, str.indexOf("#E") - 1);
+	
+				if (timestamp != "@x") {
+					/* ===== Log labels ===== */
+					if (debugMode == true) {
+						console.log("========== Row " + index + " ==========");
+						console.log("Title: [" + title + "]");
+						console.log("Desc: [" + desc + "]");
+						console.log("Date: [" + date + "]");
+						console.log("Align: [" + align + "]");
+						console.log("ImgSrc: [" + imgSrc + "]");
+						console.log("Btn: [" + btn + "]");
+						console.log("Link: [" + link + "]");
+						console.log("Flip: [" + flip + "]");
 					}
-				}
-				img.src = imgSrc;
-
-				/* ===== Add image container ===== */
-				row.append(imgContainer);
-
-				/* ===== Create gradient ===== */
-				var gradient = document.createElement("div");
-				/* ===== Set image gradient Left, Center, or Right ===== */
-				if (true) {
-					gradient.classList.add("img-gradient");
+	
+	
+					/* ===== Create row ===== */
+					var row = document.createElement('div');
+					row.classList.add('row');
+					row.id = 'row' + index;
+	
+					/* ===== Create background image ===== */
+					var imgContainer = document.createElement('div');
+					imgContainer.classList.add('img-container');
+					var img = document.createElement('img');
+					/* ===== Set image to color or null image if no image avalible ===== */
+					if (imgSrc == "") {
+						if (nullBack == "Color") {
+							img.style.backgroundColor = "black";
+						} else if (nullBack == "Img") {
+							imgSrc = "../Assets/Img/Projects/nullImg.png";
+						}
+					}
+					img.src = imgSrc;
+	
+					/* ===== Add image container ===== */
+					row.append(imgContainer);
+	
+					/* ===== Create gradient ===== */
+					var gradient = document.createElement("div");
+					/* ===== Set image gradient Left, Center, or Right ===== */
+					if (true) {
+						gradient.classList.add("img-gradient");
+						if (align == "L") {
+							gradient.classList.add('gradient-left');
+						} else if (align == "C") {
+							gradient.classList.add('gradient-center');
+						} else if (align == "R") {
+							gradient.classList.add('gradient-right');
+						}
+						imgContainer.append(gradient);
+					}
+	
+					/* ===== Flip image Horizontally, Vertically, or Both ===== */
+					if (flip == "H") {
+						img.style.transform = "scaleX(-1)";
+						gradient.style.zIndex = "1";
+					} else if (flip == "V") {
+						img.style.transform = "scaleY(-1)";
+						gradient.style.zIndex = "1";
+					} else if (flip == "B") {
+						img.style.transform = "scale(-1, -1)";
+						gradient.style.zIndex = "1";
+					}
+					/* ===== Add img ===== */
+					imgContainer.append(img);
+	
+					/* ===== Create info container===== */
+					var infoContainer = document.createElement('div');
+					infoContainer.classList.add('info-container');
+					row.append(infoContainer);
+	
+					/* ===== Align info Left, Center, or Right ===== */
 					if (align == "L") {
-						gradient.classList.add('gradient-left');
+						infoContainer.classList.add('info-left');
 					} else if (align == "C") {
-						gradient.classList.add('gradient-center');
+						infoContainer.classList.add('info-center');
 					} else if (align == "R") {
-						gradient.classList.add('gradient-right');
+						infoContainer.classList.add('info-right');
 					}
-					imgContainer.append(gradient);
-				}
-
-				/* ===== Flip image Horizontally, Vertically, or Both ===== */
-				if (flip == "H") {
-					img.style.transform = "scaleX(-1)";
-					gradient.style.zIndex = "1";
-				} else if (flip == "V") {
-					img.style.transform = "scaleY(-1)";
-					gradient.style.zIndex = "1";
-				} else if (flip == "B") {
-					img.style.transform = "scale(-1, -1)";
-					gradient.style.zIndex = "1";
-				}
-				/* ===== Add img ===== */
-				imgContainer.append(img);
-
-				/* ===== Create info container===== */
-				var infoContainer = document.createElement('div');
-				infoContainer.classList.add('info-container');
-				row.append(infoContainer);
-
-				/* ===== Align info Left, Center, or Right ===== */
-				if (align == "L") {
-					infoContainer.classList.add('info-left');
-				} else if (align == "C") {
-					infoContainer.classList.add('info-center');
-				} else if (align == "R") {
-					infoContainer.classList.add('info-right');
-				}
-
-				/* ===== Create info inner ===== */
-				var infoContainerInner = document.createElement('div');
-				infoContainerInner.classList.add('info-container-inner');
-				/* ===== Align info inner Left, Center, or Right ===== */
-				if (align == "L") {
-					infoContainerInner.classList.add('info-inner-left');
-				} else if (align == "C") {
-					infoContainerInner.classList.add('info-inner-center');
-				} else if (align == "R") {
-					infoContainerInner.classList.add('info-inner-right');
-				}
-				/* ===== Add info inner ===== */
-				infoContainer.append(infoContainerInner);
-
-				/* ===== Create info n next (info inner inner) ===== */
-				var infoNNext = document.createElement('div');
-				infoNNext.classList.add('info-n-next');
-				infoContainerInner.append(infoNNext);
-
-				/* ===== Create next ===== */
-				var next = document.createElement('div');
-				next.classList.add('next');
-				var nextIcon = document.createElement('p');
-				nextIcon.classList = "material-symbols-outlined";
-				if (btn == "U" || btn == "B") {
-					nextIcon.innerHTML = "expand_less";
-					next.setAttribute("onclick", "window.location.href = '/#row' + (parseInt($(this).parent().parent().parent().parent().attr('id').substr(3)) + 1);");
+	
+					/* ===== Create info inner ===== */
+					var infoContainerInner = document.createElement('div');
+					infoContainerInner.classList.add('info-container-inner');
+					/* ===== Align info inner Left, Center, or Right ===== */
+					if (align == "L") {
+						infoContainerInner.classList.add('info-inner-left');
+					} else if (align == "C") {
+						infoContainerInner.classList.add('info-inner-center');
+					} else if (align == "R") {
+						infoContainerInner.classList.add('info-inner-right');
+					}
+					/* ===== Add info inner ===== */
+					infoContainer.append(infoContainerInner);
+	
+					/* ===== Create info n next (info inner inner) ===== */
+					var infoNNext = document.createElement('div');
+					infoNNext.classList.add('info-n-next');
+					infoContainerInner.append(infoNNext);
+	
+					/* ===== Create next ===== */
+					var next = document.createElement('div');
+					next.classList.add('next');
+					var nextIcon = document.createElement('p');
+					nextIcon.classList = "material-symbols-outlined";
+					if (btn == "U" || btn == "B") {
+						nextIcon.innerHTML = "expand_less";
+						next.setAttribute("onclick", "window.location.href = '/#row' + (parseInt($(this).parent().parent().parent().parent().attr('id').substr(3)) + 1);");
+					} else {
+						nextIcon.innerHTML = "";
+						next.setAttribute("onclick", "");
+					}
+	
+					/* ===== Add next ===== */
+					next.append(nextIcon);
+					infoNNext.append(next);
+	
+					/* ===== Create info ===== */
+					var info = document.createElement('div');
+					info.classList.add('info');
+					/* ===== Create info title ===== */
+					var infoTitle = document.createElement('h2');
+					infoTitle.innerHTML = title;
+					infoTitle.classList = "title";
+					infoTitle.setAttribute("onclick", "window.location.href = '" + link + "'"); info.appendChild(infoTitle);
+					infoTitle.setAttribute("tabindex", index + 9)
+					/* ===== Create info description ===== */
+					var infoDesc = document.createElement('p');
+					infoDesc.innerHTML = desc;
+					infoDesc.classList = "desc";
+					info.appendChild(infoDesc);
+					/* ===== Create info Date ===== */
+					var infoDate = document.createElement('i');
+					infoDate.innerHTML = date;
+					infoDate.classList = "date";
+					info.appendChild(infoDate);
+	
+					/* ===== Add info ===== */
+					infoNNext.append(info);
+	
+					/* ===== Create Next ===== */
+					var next = document.createElement('div');
+					next.classList.add('next');
+					var nextIcon = document.createElement('p');
+					nextIcon.classList = "material-symbols-outlined";
+					if (btn == "D" || btn == "B") {
+						nextIcon.innerHTML = "expand_more";
+						next.setAttribute("onclick", "window.location.href = '/#row' + (parseInt($(this).parent().parent().parent().parent().attr('id').substr(3)) - 1);");
+					} else {
+						nextIcon.innerHTML = "";
+						next.setAttribute("onclick", "");
+					}
+					/* ===== Add next ===== */
+					next.append(nextIcon);
+					infoNNext.append(next);
+	
+					/* ===== Add row (ADD ALL) ===== */
+					$("#content").append(row);
+					
 				} else {
-					nextIcon.innerHTML = "";
-					next.setAttribute("onclick", "");
+					console.log("-----------> " + timestamp);
 				}
-
-				/* ===== Add next ===== */
-				next.append(nextIcon);
-				infoNNext.append(next);
-
-				/* ===== Create info ===== */
-				var info = document.createElement('div');
-				info.classList.add('info');
-				/* ===== Create info title ===== */
-				var infoTitle = document.createElement('h2');
-				infoTitle.innerHTML = title;
-				infoTitle.classList = "title";
-				infoTitle.setAttribute("onclick", "window.location.href = '" + link + "'"); info.appendChild(infoTitle);
-				infoTitle.setAttribute("tabindex", index + 9)
-				/* ===== Create info description ===== */
-				var infoDesc = document.createElement('p');
-				infoDesc.innerHTML = desc;
-				infoDesc.classList = "desc";
-				info.appendChild(infoDesc);
-				/* ===== Create info Date ===== */
-				var infoDate = document.createElement('i');
-				infoDate.innerHTML = date;
-				infoDate.classList = "date";
-				info.appendChild(infoDate);
-
-				/* ===== Add info ===== */
-				infoNNext.append(info);
-
-				/* ===== Create Next ===== */
-				var next = document.createElement('div');
-				next.classList.add('next');
-				var nextIcon = document.createElement('p');
-				nextIcon.classList = "material-symbols-outlined";
-				if (btn == "D" || btn == "B") {
-					nextIcon.innerHTML = "expand_more";
-					next.setAttribute("onclick", "window.location.href = '/#row' + (parseInt($(this).parent().parent().parent().parent().attr('id').substr(3)) - 1);");
-				} else {
-					nextIcon.innerHTML = "";
-					next.setAttribute("onclick", "");
-				}
-				/* ===== Add next ===== */
-				next.append(nextIcon);
-				infoNNext.append(next);
-
-				/* ===== Add row (ADD ALL) ===== */
-				$("#content").append(row);
-			} else {
-				console.log("-----------> " + timestamp);
-			}
+			// }
 		}
 	}
 }
@@ -351,7 +368,7 @@ onkeydown = (event) => {
 
 	if (event.key == "ArrowUp" && row > 0) {
 		if (window.location.href.indexOf("/#row") != -1) {
-			row = parseInt(window.location.href.slice(window.location.href.indexOf("/#row") + pageAmount)) + 1;
+			row = parseInt(window.location.href.slice(window.location.href.indexOf("/#row") + (pageAmount))) + 1;
 		} else {
 			row = 0;
 		}
@@ -359,7 +376,7 @@ onkeydown = (event) => {
 		window.location.href = "/#row" + row;
 	} else if (event.key == "ArrowDown" && row < rowAmount - 1) {
 		if (window.location.href.indexOf("/#row") != -1) {
-			row = parseInt(window.location.href.slice(window.location.href.indexOf("/#row") + pageAmount)) - 1;
+			row = parseInt(window.location.href.slice(window.location.href.indexOf("/#row") + (pageAmount))) - 1;
 		} else {
 			row = 1;
 		}
@@ -374,7 +391,7 @@ function prevPage() {
 		index = index - (pageAmount * pageNumber);
 		pageNumber = pageNumber - 1;
 		createContent();
-		window.location.href = "/#row" + (index - rowAmount);
+		window.location.href = "/#row" + ((index + pageAmount));
 		setPageText();
 	}
 }
@@ -385,7 +402,7 @@ function nextPage() {
 		index = index - (pageAmount * pageNumber);
 		pageNumber = pageNumber + 1;
 		createContent();
-		window.location.href = "/#row" + (index - rowAmount);
+		window.location.href = "/#row" + ((index + pageAmount));
 		setPageText();
 	}
 }
