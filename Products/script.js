1b0c25eabad63eaee0c100f35312148442aefba4
spectronindindustries <spectronind@gmail.com>
2024-10-01 20:20:27 -0600

    Fixed typo in date products

--- Products/script.js
+++ Products/script.js
@@ -4,7 +4,7 @@ var products = [
         img: "../Assets/Img/Projects/AiteModulePrototypev3.jpeg",
         desc: "Project Aite is the first prototype of the state of art hologram technology. It uses particles in the air reflecting light to create an image. Version 1 has 25 pixels. Each pixel is a module that has a light in it. A motherboard controls which pixel should be on and off to create the image you want. It increases the brightness to let the pixel go farther than other pixels.",
         url: "Order/",
-        date: "09 / 2037",
+        date: "09 / 2023",
         price: 15.99
     },
 
